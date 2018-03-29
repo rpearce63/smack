1dc047cb05b159887a9e82079e4797a9baab6e97
Rick <rick@server.local>
2018-03-28 22:04:56 -0500

    login screen

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Rick on 3/28/18.
+//  Copyright © 2018 Rick Pearce. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func loginPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Rick on 3/28/18.
+//  Copyright © 2018 Rick Pearce. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"

0e844636fff06849717a6fc5e25a75f987e35d5a
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-03-29 14:15:18 -0500

    lesson 11

--- smack/Controller/CreateAccountVC.swift
+++ smack/Controller/CreateAccountVC.swift
@@ -27,7 +27,9 @@ class CreateAccountVC: UIViewController {
         
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
-                print("registered user!")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    print("logged in user!", AuthService.instance.authToken)
+                })
             }
         }
     }

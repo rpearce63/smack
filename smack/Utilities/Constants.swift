ae216ddb32974ce3ac2301ee63e26c7efebb6cc5
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-03-29 11:27:10 -0500

    lesson 9

--- smack/Utilities/Constants.swift
+++ smack/Utilities/Constants.swift
@@ -8,8 +8,21 @@
 
 import Foundation
 
-// Segues
+//custom closure for web request
+typealias CompletionHandler = (_ Success: Bool) -> ()
 
+// URL Constants
+let BASE_URL = "https://mac-chat-crzs.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+// Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+

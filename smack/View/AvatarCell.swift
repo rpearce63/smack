65b1b496882069eda1d7ff6c42b602f43784fba8
Rick <rick@server.local>
2018-03-29 22:11:21 -0500

    lesson 13

---
+++ smack/View/AvatarCell.swift
@@ -0,0 +1,27 @@
+//
+//  AvatarCell.swift
+//  smack
+//
+//  Created by Rick on 3/29/18.
+//  Copyright © 2018 Rick Pearce. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+    
+}

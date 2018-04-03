6a23f77dcb0be7772d9d88d2ecbf466d4020a291
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-04-03 16:31:26 -0500

    timestamp

--- smack/View/MessageCell.swift
+++ smack/View/MessageCell.swift
@@ -26,5 +26,21 @@ class MessageCell: UITableViewCell {
         userNameLbl.text = message.userName
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = String(isoDate[..<end])
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormmater = DateFormatter()
+        newFormmater.dateFormat = "MMM d, h:mm a"
+        if let finalDate = chatDate {
+            let finalDate = newFormmater.string(from: finalDate)
+            timeStampLbl.text = finalDate
+        }
+        
+        
     }
 }

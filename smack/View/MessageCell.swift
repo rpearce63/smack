73eac6cdd44f8f6df963108d6fc1c31ef507f6b2
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-04-03 12:07:09 -0500

    display message

---
+++ smack/View/MessageCell.swift
@@ -0,0 +1,31 @@
+//
+//  MessageCell.swift
+//  smack
+//
+//  Created by Rick Pearce on 4/3/18.
+//  Copyright © 2018 Rick Pearce. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    //Outlets
+    @IBOutlet weak var userImage: CircleImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timeStampLbl: UILabel!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configureCell(message: Message) {
+        messageBodyLbl.text = message.message
+        userNameLbl.text = message.userName
+        userImage.image = UIImage(named: message.userAvatar)
+        userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+}
